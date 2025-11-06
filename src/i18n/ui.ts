7a9c8eee69d266f297ee8493d0735c9d672bf018
Jesus Mendoza <albertomendozax9800@gmail.com>
2025-11-05 19:32:30 -0600

    added english translation for home page

---
+++ src/i18n/ui.ts
@@ -0,0 +1,63 @@
+export const languages = {
+    en: 'English',
+    es: 'Español',
+};
+
+export const defaultLang = 'en';
+export const showDefaultLang = false;
+
+export const ui = {
+    en: {
+
+        // Header
+        'header.about': 'About',
+        'header.menu': 'Menu',
+        'header.location': 'Location',
+
+        // Hero
+        'hero.title': 'Serving Yucatán to you',
+        'hero.button': 'Reserve',
+
+        // About
+        'about.title': 'About K’i’ik',
+        'about.firstp': 'At K’i’ik, guests are invited to indulge in a refined culinary journey set within the timeless charm of Mérida’s historic center. Our restaurant combines world-class gastronomy with the warmth and elegance of Yucatecan architecture, creating an atmosphere that is both luxurious and welcoming.',
+        'about.secondp': 'Every dish is crafted from the finest locally sourced ingredients, paired with carefully selected international products to deliver flavors that are both authentic and innovative. From attentive service to a thoughtfully designed space, K’i’ik is dedicated to providing a complete dining experience that is memorable.',
+
+        // Menu
+        'menu.title': 'MENU',
+        'menu.desc': 'Culinary artistry and passionate service come together in Mérida’s historic heart, offering a dining experience that blends local heritage with modern sophistication.',
+        'menu.button': 'MENU',
+
+        // Location
+        'location.title': 'LOCATION',
+        'location.desc': 'BASED IN THE HEART OF MÉRIDA, YUCATÁN. HISTORIC CENTER.',
+        'location.button': 'DIRECTIONS'
+
+    },
+    es: {
+
+        // Header
+        'header.about': 'Sobre K’i’ik',
+        'header.menu': 'Menú',
+        'header.location': 'Ubicación',
+
+        // Hero
+        'hero.title': 'Sirviéndote Yucatán',
+        'hero.button': 'Reservar',
+
+        // About
+        'about.title': 'Acerca de K’i’ik',
+        'about.firstp': 'En K’i’ik, los invitados están invitados a disfrutar de un viaje culinario refinado enmarcado en el encanto atemporal del centro histórico de Mérida. Nuestro restaurante combina gastronomía de clase mundial con la calidez y elegancia de la arquitectura yucateca, creando una atmósfera tanto lujosa como acogedora.',
+        'about.secondp': 'Cada platillo se elabora con los mejores ingredientes locales, complementados con productos internacionales cuidadosamente seleccionados para ofrecer sabores que son tanto auténticos como innovadores. Desde el servicio atento hasta un espacio diseñado con esmero, K’i’ik se dedica a brindar una experiencia gastronómica completa e inolvidable.',
+
+        // Menu
+        'menu.title': 'MENÚ',
+        'menu.desc': 'El arte culinario y el servicio apasionado se unen en el corazón histórico de Mérida, ofreciendo una experiencia gastronómica que combina la herencia local con la sofisticación moderna.',
+        'menu.button': 'MENÚ',
+
+        // Location
+        'location.title': 'UBICACIÓN',
+        'location.desc': 'EN EL CORAZÓN DE MÉRIDA, YUCATÁN. CENTRO HISTÓRICO.',
+        'location.button': 'CÓMO LLEGAR'
+    }
+} as const;
